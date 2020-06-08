b003cf082a8fdbd5cbdf2fb22f0f69a2f8528d4a
miroyu1991 <46637440+miroyu1991@users.noreply.github.com>
2020-06-07 22:50:12 -0400

    Bubble Sort Select Sort

---
+++ src/Changjiang/sort/BubbleSort.java
@@ -0,0 +1,58 @@
+package Changjiang.sort;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = new int[80000];
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = (int)(Math.random()*10000);
+        }
+
+        // test time used
+        long startTime = System.currentTimeMillis();
+
+        bubbleSort(arr);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Bubble Sort uses " + (endTime - startTime) + "ms to finish sort");
+        //System.out.println(Arrays.toString(arr));
+    }
+
+    /*
+        Theory:
+        1.For all number in the array, start from the first number and end of the last number - 1
+        compare the number and next of this number, if meet exchange condition, exchange these numbers
+        Finally get the last round time numbers in order
+        For example: if run step [1] 5 times, I will get 5 numbers in the right position
+        This is a round
+        2. If last round change the position of numbers, start next round
+        Until every number is in order:
+            No more exchange or round arr.length - 1 times.
+
+        O(n^2)
+     */
+    public static void bubbleSort(int[] arr){
+        int temp = 0;
+        boolean flag = false; //use to judge if the array needs another round of sort
+
+        for(int i = 0; i < arr.length -1; i++){
+            for(int j = 0; j < arr.length -1 -i; j++){
+                if(arr[j] > arr[j+1]){
+
+                    // if arr[j] > arr[j+1], exchange these two numbers and set flag to true
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    flag = true;
+                }
+            }
+
+            if(flag){
+                // If exchange numbers, reset flag
+                flag = false;
+            }else{
+                // If not exchange numbers, exit the for-loop
+                break;
+            }
+        }
+    }
+}
---
+++ src/Changjiang/sort/SelectSort.java
@@ -0,0 +1,53 @@
+package Changjiang.sort;
+
+public class SelectSort {
+    public static void main(String[] args) {
+        int[] arr = new int[80000];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * 10000);
+        }
+
+        //System.out.println(Arrays.toString(arr));
+        // test time that used
+        long startTime = System.currentTimeMillis();
+
+        selectSort(arr);
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Select Sort uses " + (endTime - startTime) + "ms to finish sort");
+        //System.out.println(Arrays.toString(arr));
+    }
+
+    /*
+        Theory:
+        1. From arr[0] to arr[n-1], find min number,
+        exchange this number with arr[0]
+        2. From arr[1] to arr[n-1], repeat step [1]
+
+        O(n^2)
+     */
+    public static void selectSort(int[] arr) {
+
+        int index;
+        int temp;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            //set index to the first element in the range
+            index = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[index] > arr[j]) {
+                    // find a smaller number
+                    index = j;
+                }
+            }
+
+            // after a round, exchange the smallest number in range with arr[i]
+            temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+
+    }
+}
